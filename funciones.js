9e66cb8004e8e2996318c208b09682842e9a149f
XimenaTania <xime.tania17@gmail.com>
2022-08-01 19:58:06 -0600

    arreglo de sombras

--- funciones.js
+++ funciones.js
@@ -5,45 +5,44 @@ const guardarContacto = (guarda, contacto) => {
 }
 const cargarContacto = (guarda, parentNode) => {
     let claves = Object.keys(guarda)
-
     for (clave of claves) {
         let contacto = JSON.parse(guarda.getItem(clave))
         crearContacto(parentNode, contacto, guarda)
+
+
     }
 }
 const crearContacto = (parentNode, contacto, guarda) => {
     let divContacto = document.createElement('div')
-    let nombreContacto = document.createElement('p')
-    let apellidoContacto = document.createElement('p')
-    let numeroContacto = document.createElement('p')
-    let correoContacto = document.createElement('p')
-    let iconBorrar = document.createElement('span')
-
+    let nombreContacto = document.createElement('h3')
+    let apellidoContacto = document.createElement('h3')
+    let numeroContacto = document.createElement('h3')
+    let correoContacto = document.createElement('h3')
+    let iconoBorrar = document.createElement('span')
 
     nombreContacto.innerHTML = contacto.nombre
     apellidoContacto.innerHTML = contacto.apellido
     numeroContacto.innerHTML = contacto.numero
     correoContacto.innerHTML = contacto.correo
-    iconBorrar.innerHTML = 'delete_forever'
+    iconoBorrar.innerHTML = 'delete'
+
 
     divContacto.classList.add('tarea')
-    divContacto.classList.add('material-symbols-outlined', 'icono')
+    iconoBorrar.classList.add('material-symbols-outlined', 'icono')
 
 
-    iconBorrar.onclick = () => {
+    iconoBorrar.onclick = () => {
         guarda.removeItem(contacto.id)
         window.location.href = '/'
 
     }
 
-
     divContacto.appendChild(nombreContacto)
     divContacto.appendChild(apellidoContacto)
-    divContacto.appendChild(correoContacto)
     divContacto.appendChild(numeroContacto)
-    divContacto.appendChild(iconBorrar)
+    divContacto.appendChild(correoContacto)
+    divContacto.appendChild(iconoBorrar)
 
     parentNode.appendChild(divContacto)
-
 
 }
